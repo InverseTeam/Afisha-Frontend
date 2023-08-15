23a821c8047fdb85605026ab4382fae9a40d582c
”OraPoulpe” <ssazonov69@gmail.com>
2023-08-15 14:50:11 +0500

    add hover and click effect for button

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,9 +5,10 @@ import { MainButton } from '@/entities/buttons/mainButton/ui/ui';
 import { IconButton } from '@/entities/buttons/iconButton/ui/ui';
 import iconPlus from '../../../public/icon/Plus.svg';
 import { TextInput } from '@/entities/inputs/textInput';
+import { AuthButton } from '@/entities/buttons/authButton/ui/ui';
 
 export default function Test() {
-const [text, setText] = useState<string> ('')
+  const [text, setText] = useState<string>('');
   return (
     <div>
       <MainButton
@@ -15,19 +16,37 @@ const [text, setText] = useState<string> ('')
         height="large"
         color="#528D3D"
         textColor="white"
+        isActive
         onClick={() => {
           console.log('ghghg');
         }}>
-        Pfhfh
+        Push
       </MainButton>
-      <IconButton height="46px" width="46px" iconSrc={iconPlus.src} color="#528D3D" onClick={() => {}} />
-      <TextInput inputStyle={{ width: '100%' }}
-                        inputName="Ответ"
-                        inputPlaceholder="Ответ"
-                        inputId="Ответ"
-                        inputTypes="text"
-                        inputValue={text}
-                        setText={setText}/>
+      <IconButton
+        height="46px"
+        width="46px"
+        iconSrc={iconPlus.src}
+        color="#528D3D"
+        onClick={() => { console.log('icon')}}
+      />
+      <TextInput
+        inputStyle={{ width: '100%' }}
+        inputName="Ответ"
+        inputPlaceholder="Ответ"
+        inputId="Ответ"
+        inputTypes="text"
+        inputValue={text}
+        setText={setText}
+      />
+
+      <AuthButton
+        onClick={()=>{}}
+        width="fit-content"
+        btnStyle={{ marginBottom: '8px' }}
+        use={'disabled'}
+        height="large">
+        Опубликовать
+      </AuthButton>
     </div>
   );
 }
