e155123c7c441bc3e3a95569eb13172351c35696
Dmitriy <dimastepanov.1995@mail.ru>
2023-08-17 22:55:28 +0500

    add publishedGetLink & unpublishedGetLink

--- src/features/cards/publishedState/model/index.ts
+++ src/features/cards/publishedState/model/index.ts
@@ -2,7 +2,7 @@ import { instanceLogged } from '@/shared/api/axios';
 
 export const Get = async () => {
   try {
-    const getEvent: any = await instanceLogged.get('/events/');
+    const getEvent: any = await instanceLogged.get('/events/published/');
     return getEvent.data;
   } catch (error) {
     return error;
