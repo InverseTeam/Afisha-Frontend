510a652fb1f23abd4e38a077d3c95fc7fd9d4d40
Dmitriy <dimastepanov.1995@mail.ru>
2023-08-17 16:43:21 +0500

    cgange AuthLayoutComponent, add security for pages admin/*

--- src/app/admin/event/page.tsx
+++ src/app/admin/event/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { Gapped } from '@/shared/gapped/ui/ui';
+import { AuthLayout } from '@/shared/layouts/authLayout';
 
 import { EventForm } from '@/widgets/cardsRender';
 
 export default function Home() {
   return (
     <>
-      <Gapped style={{ width: '100%', display: 'flex' }} vertical gap="32px">
-        <EventForm />
-      </Gapped>
+      <AuthLayout isAuth blockPageLink="/admin/event">
+        <Gapped style={{ width: '100%', display: 'flex' }} vertical gap="32px">
+          <EventForm />
+        </Gapped>
+      </AuthLayout>
     </>
   );
 }
